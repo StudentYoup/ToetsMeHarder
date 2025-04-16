96542c8268fdf3fea1af3925353a9ba85d94b648
GerritScholten <s1199182@student.windesheim.nl>
2025-04-16 19:48:54 +0200

    Added project structure
    Structuur zoals we geleerd hebben volgens OOP

---
+++ ToetsMeHarder/ToetsMeHarder.PianoGUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ToetsMeHarder.PianoGUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
