806d242ecff356b4bd25183e24b4be47c02b41e8
GerritScholten <s1199182@student.windesheim.nl>
2025-04-23 20:29:09 +0200

    removed unwanted projects

--- ToetsMeHarder/PianoProject/Note.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PianoProject;
-
-public class Note(int noteID, int pitch,int duration)
-{
-    public int Pitch { get; private init; } = pitch;
-    public int Duration { get; private init; } = duration;
-    public int NoteID { get; private init; } = noteID;
-}
