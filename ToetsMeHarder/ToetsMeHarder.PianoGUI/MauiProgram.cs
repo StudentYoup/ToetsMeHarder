7434eb494ec4e132a07f6af74ce7eed2a580bea2
Wessel te Riele <ikweswes@gmail.com>
2025-04-24 11:39:26 +0200

    fix

--- ToetsMeHarder/ToetsMeHarder.PianoGUI/MauiProgram.cs
+++ ToetsMeHarder/ToetsMeHarder.PianoGUI/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Plugin.Maui.Audio;
 
 namespace ToetsMeHarder.PianoGUI;
 
@@ -15,6 +16,7 @@ public static class MauiProgram
 			});
 
 		builder.Services.AddMauiBlazorWebView();
+		builder.Services.AddSingleton(AudioManager.Current);
 
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
