69b6047c7cf56a9408f19835ec32883c4404a5c7
GerritScholten <s1199182@student.windesheim.nl>
2025-04-25 10:56:49 +0200

    Moved sound logic to GUI layer

--- ToetsMeHarder/ToetsMeHarder.Business/Metronome/IMetronomeService.cs
+++
@@ -1,8 +0,0 @@
-public interface IMetronomeService
-{
-    int BPM { get; set; }
-    bool IsRunning { get; }
-    event EventHandler Beat;
-    void Start();
-    void Stop();
-}
--- ToetsMeHarder/ToetsMeHarder.PianoGUI/Components/Pages/Metronome.razor.cs
+++ ToetsMeHarder/ToetsMeHarder.PianoGUI/Components/Pages/Metronome.razor.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using ToetsMeHarder.Business;
+using Plugin.Maui.Audio;
 
 namespace ToetsMeHarder.PianoGUI.Pages
 {
@@ -7,6 +8,24 @@ namespace ToetsMeHarder.PianoGUI.Pages
     {
         [Inject]
         public MetronomeService metronome { get; set; } = default!; // moet injecteren vanuit mauiProgram anders werkt het niet :-)
+
+        [Inject] 
+        public IAudioManager AudioManager { get; set; } = default!; // same
+
+        private IAudioPlayer _player;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var file = await FileSystem.OpenAppPackageFileAsync(@"Resources\Sound\metronoom.mp3");
+            _player = AudioManager.CreatePlayer(file);
+            metronome.Beat += OnBeat;
+        }
+
+        private void OnBeat(object? sender, EventArgs e)
+        {
+            _player?.Play();
+        }
+
 
 
         protected string BpmText
