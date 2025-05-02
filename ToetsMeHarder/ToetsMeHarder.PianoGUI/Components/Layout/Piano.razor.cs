e75f88d9c2faba42e7929ff86b28fdaaeaed1086
StudentYoup <s1198594@student.windesheim.nl>
2025-05-02 15:32:25 +0200

    FIX
    geen dubbele code
    visuals

--- ToetsMeHarder/ToetsMeHarder.PianoGUI/Components/Layout/Piano.razor.cs
+++ ToetsMeHarder/ToetsMeHarder.PianoGUI/Components/Layout/Piano.razor.cs
@@ -2,76 +2,79 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Maui.Controls;
 
 
-namespace ToetsMeHarder.PianoGUI.Components.Layout{
-
-public partial class Piano
+namespace ToetsMeHarder.PianoGUI.Components.Layout
 {
-    private AudioHandler _audioHandler = new AudioHandler();
-
-    private const int _NOTE_DURATION = 1000;
+    public partial class Piano
+    {
+        private AudioHandler _audioHandler = new AudioHandler();
     
-      private HashSet<string> _pressedKeys = new HashSet<string>();
+        private HashSet<string> _pressedKeys = new HashSet<string>();
 
-    private readonly Dictionary<string, Note> _pianoKeys = new()
-    {
-        ["1"] = new Note(110.00, _NOTE_DURATION),
-        ["2"] = new Note(116.54, _NOTE_DURATION),
-        ["3"] = new Note(123.47, _NOTE_DURATION),
-        ["4"] = new Note(130.81, _NOTE_DURATION),
-        ["5"] = new Note(138.59, _NOTE_DURATION),
-        ["6"] = new Note(146.83, _NOTE_DURATION),
-        ["7"] = new Note(155.56, _NOTE_DURATION),
-        ["8"] = new Note(164.81, _NOTE_DURATION),
-        ["9"] = new Note(174.61, _NOTE_DURATION),
-        ["0"] = new Note(185.00, _NOTE_DURATION),
-        ["q"] = new Note(196.00, _NOTE_DURATION),
-        ["w"] = new Note(207.65, _NOTE_DURATION),
-        ["e"] = new Note(220.00, _NOTE_DURATION),
-        ["r"] = new Note(233.08, _NOTE_DURATION),
-        ["t"] = new Note(246.94, _NOTE_DURATION),
-        ["y"] = new Note(261.63, _NOTE_DURATION),
-        ["u"] = new Note(277.18, _NOTE_DURATION),
-        ["i"] = new Note(293.66, _NOTE_DURATION),
-        ["o"] = new Note(311.13, _NOTE_DURATION),
-        ["p"] = new Note(329.63, _NOTE_DURATION),
-        ["a"] = new Note(349.23, _NOTE_DURATION),
-        ["s"] = new Note(369.99, _NOTE_DURATION),
-        ["d"] = new Note(392.00, _NOTE_DURATION),
-        ["f"] = new Note(415.30, _NOTE_DURATION),
-        ["g"] = new Note(440.00, _NOTE_DURATION),
-        ["h"] = new Note(466.16, _NOTE_DURATION),
-        ["j"] = new Note(493.88, _NOTE_DURATION),
-        ["k"] = new Note(523.25, _NOTE_DURATION),
-        ["l"] = new Note(554.37, _NOTE_DURATION),
-        [";"] = new Note(587.33, _NOTE_DURATION),
-        ["'"] = new Note(622.25, _NOTE_DURATION),
-        ["z"] = new Note(659.26, _NOTE_DURATION),
-        ["x"] = new Note(698.46, _NOTE_DURATION),
-        ["c"] = new Note(739.99, _NOTE_DURATION),
-        ["v"] = new Note(783.99, _NOTE_DURATION),
-        ["b"] = new Note(830.61, _NOTE_DURATION),
-        ["n"] = new Note(880.00, _NOTE_DURATION),
-        ["m"] = new Note(932.33, _NOTE_DURATION),
-        [","] = new Note(987.77, _NOTE_DURATION),
-        ["."] = new Note(1046.50, _NOTE_DURATION),
-    };
-
-    public void HandleKeyDown(KeyboardEventArgs e)
-    {
-        if(_pianoKeys.ContainsKey(e.Key) && !_pressedKeys.Contains(e.Key))
+        private readonly Dictionary<string, string> _pianoKeys = new()
         {
-            _audioHandler.PlayAudio(_pianoKeys[e.Key]);
-            _pressedKeys.Add(e.Key);
+            ["q"] = "a2",
+            ["2"] = "a#2",
+            ["w"] = "b2",
+            ["e"] = "c3",
+            ["4"] = "c#3",
+            ["r"] = "d3",
+            ["5"] = "d#3",
+            ["t"] = "e3",
+            ["y"] = "f3",
+            ["7"] = "f#3",
+            ["u"] = "g3",
+            ["8"] = "g#3",
+            ["i"] = "a3",
+            ["9"] = "a#3",
+            ["o"] = "b3",
+            ["p"] = "c4",
+            ["-"] = "c#4",
+            ["["] = "d4",
+            ["="] = "d#4",
+            ["]"] = "e4",
+            ["z"] = "f4",
+            ["s"] = "f#4",
+            ["x"] = "g4",
+            ["d"] = "g#4",
+            ["c"] = "a4",
+            ["f"] = "a#4",
+            ["v"] = "b4",
+            ["b"] = "c5",
+            ["h"] = "c#5",
+            ["n"] = "d5",
+            ["j"] = "d#5",
+            ["m"] = "e5",
+            [","] = "f5",
+            ["l"] = "f#5",
+            ["."] = "g5",
+            [";"] = "g#5",
+            ["/"] = "a5"
+            // [""] = "a#5",
+            // [""] = "b5",
+            // [""] = "c6",
+        };
+
+        [Inject] private IJSRuntime JSRuntime { get; set; }
+        public void HandleKeyDown(KeyboardEventArgs e)
+        {
+            if (_pianoKeys.ContainsKey(e.Key) && !_pressedKeys.Contains(e.Key))
+            {
+                var noteId = _pianoKeys[e.Key];
+                _pressedKeys.Add(e.Key);
+                JSRuntime.InvokeVoidAsync("setKeyActive", noteId);
+            }
+        }
+        public void HandleKeyUp(KeyboardEventArgs e)
+        {
+            if (_pianoKeys.ContainsKey(e.Key) && _pressedKeys.Contains(e.Key))
+            {
+                var noteId = _pianoKeys[e.Key];
+                _pressedKeys.Remove(e.Key);
+                JSRuntime.InvokeVoidAsync("setKeyInactive", noteId);
+            }
         }
     }
-    public void HandleKeyUp(KeyboardEventArgs e)
-    {
-        if(_pianoKeys.ContainsKey(e.Key) && _pressedKeys.Contains(e.Key))
-        {
-            _pressedKeys.Remove(e.Key);
-        }
-    }
-}
 }
