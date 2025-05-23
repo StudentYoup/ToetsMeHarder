264fff81add3683f716e8dd55fbeeece1c510d3e
Dekobra-NR <144335547+Dekobra-NR@users.noreply.github.com>
2025-05-23 13:09:38 +0200

    laatste stuk gedaan in files
    alles in de files is als het goed is vertaald naar het engels en alles lijkt nog te werken.

--- ToetsMeHarder/ToetsMeHarder.PianoGUI/Components/Pages/LiedjesKeuzeMenu.razor.cs
+++ ToetsMeHarder/ToetsMeHarder.PianoGUI/Components/Pages/LiedjesKeuzeMenu.razor.cs
@@ -5,17 +5,17 @@ namespace ToetsMeHarder.PianoGUI.Components.Pages;
 
 public partial class LiedjesKeuzeMenu_razor : ComponentBase
 {
-    private LiedjesManager _liedjesManager = LiedjesManager.Instance;
+    private LiedjesManager _songManager = LiedjesManager.Instance;
     Songs song = new Songs("TEST",120);
     
     [Parameter] public EventCallback OnOpen { get; set;}
     [Parameter] public EventCallback OnClose { get; set;}
-    [Parameter] public bool IsOpen { get; set; } = false;
-    [Parameter] public EventCallback LiedjeGekozen { get; set; }
-    public async Task SetLiedje()
+    [Parameter] public bool IsOpen { get; set;} = false;
+    [Parameter] public EventCallback SongChosen { get; set;}
+    public async Task SetSong()
     {
-        _liedjesManager.ChosenSong = song;
-        await LiedjeGekozen.InvokeAsync(); // Notify parent to close pop-up
+        _songManager.ChosenSong = song;
+        await SongChosen.InvokeAsync(); // Notify parent to close pop-up
     }
 
     public async void Open()
