13b1f4489fb770bfc0bd9cf12e79e89b2f45b219
Rick Hesp <s1179208@student.windesheim.nl>
2025-05-26 00:36:26 +0200

    styling on the vertical bars is fixed, now error on th loading of the falling blocks

--- ToetsMeHarder/ToetsMeHarder.PianoGUI/Components/Layout/FallingBlocks.razor.cs
+++ ToetsMeHarder/ToetsMeHarder.PianoGUI/Components/Layout/FallingBlocks.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using ToetsMeHarder.Business;
+using ToetsMeHarder.Business.FallingBlocks;
 using ToetsMeHarder.Business.LiedjesComponent;
 using ToetsMeHarder.PianoGUI.Pages;
 
@@ -14,6 +15,52 @@ namespace ToetsMeHarder.PianoGUI.Components.Layout
         private Random _random = new();
         private Dictionary<int, List<int>> _blockMap = new();
         private int _numberOfBars = 40;
+        private KeyValue key = new KeyValue();
+        private int x = 0;
+
+        private readonly Dictionary<KeyValue, double> _noteFrequencies = new Dictionary<KeyValue, double>
+        {
+            { KeyValue.a2, 110.00 },
+            { KeyValue.a21, 116.54 },
+            { KeyValue.b2, 123.47 },
+            { KeyValue.c3, 130.81 },
+            { KeyValue.c31, 138.59 },
+            { KeyValue.d3, 146.83 },
+            { KeyValue.d31, 155.56 },
+            { KeyValue.e3, 164.81 },
+            { KeyValue.f3, 174.61 },
+            { KeyValue.f31, 185.00 },
+            { KeyValue.g3, 196.00 },
+            { KeyValue.g31, 207.65 },
+            { KeyValue.a3, 220.00 },
+            { KeyValue.a31, 233.08 },
+            { KeyValue.b3, 246.94 },
+            { KeyValue.c4, 261.63 },
+            { KeyValue.c41, 277.18 },
+            { KeyValue.d4, 293.66 },
+            { KeyValue.d41, 311.13 },
+            { KeyValue.e4, 329.63 },
+            { KeyValue.f4, 349.23 },
+            { KeyValue.f41, 369.99 },
+            { KeyValue.g4, 392.00 },
+            { KeyValue.g41, 415.30 },
+            { KeyValue.a4, 440.00 },
+            { KeyValue.a41, 466.16 },
+            { KeyValue.b4, 493.88 },
+            { KeyValue.c5, 523.25 },
+            { KeyValue.c51, 554.37 },
+            { KeyValue.d5, 587.33 },
+            { KeyValue.d51, 622.25 },
+            { KeyValue.e5, 659.26 },
+            { KeyValue.f5, 698.46 },
+            { KeyValue.f51, 739.99 },
+            { KeyValue.g5, 783.99 },
+            { KeyValue.g51, 830.61 },
+            { KeyValue.a5, 880.00 },
+            { KeyValue.a51, 932.33 },
+            { KeyValue.b5, 987.77 },
+            { KeyValue.c6, 1046.50 }
+        };
 
         protected override void OnInitialized()
         {
@@ -38,6 +85,12 @@ namespace ToetsMeHarder.PianoGUI.Components.Layout
                 StateHasChanged();
                 await Task.Delay(60_000 / Metronome.BPM); // Timing = 1min / bpm
             }
+        }
+
+        private string CreateCSSClass(string key)
+        {
+            string color = key.Contains("1") ? "black " : "white ";
+            return $"vertical-bar-{color}";// bijvoorbeeld: "black-bar" of "white-bar"
         }
 
 
