5906f63da41a61e12516f25f15e1495f890cf51d
Wessel te Riele <ikweswes@gmail.com>
2025-05-26 11:36:36 +0200

    DB connection function
    you can connect and disconnect from the db considering that you are connected to our vpn

---
+++ ToetsMeHarder/ToetsMeHarder.Business/IDataManager.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ToetsMeHarder.Business;
+
+public interface IDataManager
+{
+    public SqlConnection Connection { get; }
+    public void Connect();
+    public void Disconnect();
+}
